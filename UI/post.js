95a46f9dedc16c016b265bd57761ae491e2abaf9
DariaBorisevich <45313223+Borrya@users.noreply.github.com>
2019-05-23 00:05:30 +0300

    Task 7

--- UI/post.js
+++ UI/post.js
@@ -1,17 +1,36 @@
 
 class Post {
 
-    constructor(photoPost){
-        
-        this.photoPost = photoPost;
+    constructor(author = '', photoLink = '', description = '', 
+    hashTags = [], createdAt = new Date(), likes = [], id = -1, numberLikes = likes.length, comments = []) {
 
-    }
+    this.author = author;
+    this.photoLink = photoLink;
+    this.description = description;
+    this.id = id;
+    this.createdAt = createdAt;
+    this.likes = likes;
+    this.hashTags = hashTags;
+    this.numberLikes = numberLikes ? numberLikes : 0;
+    this.comments = comments;
+}
+
+    static parseToPost(object) {
+
+		return new Post(
+            object.author,
+            object.photoLink,
+			object.description,
+			object.hashTags,
+			new Date(Date.parse(object.createdAt)),
+			object.likes,
+			object.id,
+			object.numberLikes,
+			object.comments
+		);
+	}
 
     static validatePhotoPost(photoPost){
-
-        if((photoPost.id === "") || typeof(photoPost.id) !== typeof('string')){
-            return false;
-        }
     
         if(!(photoPost.createdAt instanceof Date)){
             return false;
@@ -24,14 +43,24 @@ class Post {
         if((photoPost.photoLink === "") || typeof(photoPost.photoLink) !== typeof('string')){
             return false;
         }
-    
-        if(photoPost.hashTags.length === 0  || !Array.isArray(photoPost.hashTags)){
-            return false;
-        }
-    
-        if((photoPost.likes === "") || typeof(photoPost.likes) !== typeof('string')){
-            return false;
-        }
+        
         return true;
     }
+
+    likedPost(userName) {
+
+		let index = this.liked.indexOf(userName);
+
+		if (index === -1) {
+			this.liked.push(userName);
+		} else {
+			this.liked.splice(index, 1);
+		}
+	}
+
+	ifLiked(userName) {
+		return (this.liked.some(function(item) {
+			return (item === userName);
+		}));
+	}
 }
