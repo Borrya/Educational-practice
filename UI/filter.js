658002aa1ffc3364473c31beea85c4500ced2d88
DariaBorisevich <45313223+Borrya@users.noreply.github.com>
2019-05-23 00:15:54 +0300

    Task 7

--- UI/filter.js
+++ UI/filter.js
@@ -1,40 +1,5 @@
 class Filter {
     
-    static getFilters(){
-
-        let filter = document.getElementById('filter');
-
-        filter.innerHTML = `
-        <span class="Filter">
-                <p><strong>Filter</strong></p>
-            </span>
-
-            <p>By name</p>
-
-            <div class="onoffswitch">
-                <input type="checkbox" name="onoffswitch" class="onoffswitch-checkbox" id="myonoffswitch" checked>
-                <label class="onoffswitch-label" for="myonoffswitch"></label>
-            </div>
-
-            <p>By date</p>
-
-            <div class="onoffswitch2">
-                <input type="checkbox" name="onoffswitch2" class="onoffswitch2-checkbox" id="myonoffswitch2" checked>
-                <label class="onoffswitch2-label" for="myonoffswitch2"></label>
-            </div>
-
-            <p>By hashtags</p>
-
-            <div class="onoffswitch3">
-                <input type="checkbox" name="onoffswitch3" class="onoffswitch3-checkbox" id="myonoffswitch3" checked>
-                <label class="onoffswitch3-label" for="myonoffswitch3">
-                <span class="onoffswitch3-inner"></span>
-        <span class="onoffswitch3-switch"></span>
-        </label>
-            </div>
-        `;
-    }
-    
     constructor(f_author = '', f_hashtags = [], f_date = true) {
 		this.f_author = f_author;
 		this.f_hashtags = f_hashtags;
@@ -51,7 +16,7 @@ class Filter {
 
 	validateFilter() {
 
-		if (typeof (this.f_author) !== 'string' || !isStringArray(this.f_hashtags)) {
+		if (typeof (this.f_author) !== 'string') {
 			return false;
 		}
 		return true;
